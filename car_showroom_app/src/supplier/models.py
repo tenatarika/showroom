69e0520c6e50a170e11674be0ee18d36a7e16ab8
MacFurcas <faracsit@gmail.com>
2021-10-11 11:59:36 +0300

    add models Car and Supplier

--- car_showroom_app/src/supplier/models.py
+++ car_showroom_app/src/supplier/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    """Car model
+    """
+
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, max_length=255)
+    width = models.IntegerField(blank=True)
+    mileage = models.IntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Supplier(models.Model):
+    """provider
+    """
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, max_length=255)
+    balance = models.DecimalField(max_digits=14, decimal_places=2)
+    car = models.ManyToManyField(Car, through='Supplier_Car')
+
+    def __str__(self):
+        return self.name
+
+
+class Supplier_Car(models.Model):
+    """Buying cars
+    """
+    count = models.IntegerField(default=1)
+    discount = models.DecimalField(max_digits =10, decimal_places=2, default=0)
+    date = models.DateTimeField(auto_now=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
