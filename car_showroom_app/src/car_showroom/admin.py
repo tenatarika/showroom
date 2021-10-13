ba1232a156f84cf2d0b93409f8153e90f27537b0
MacFurcas <faracsit@gmail.com>
2021-10-13 14:25:20 +0300

    update admin

--- car_showroom_app/src/car_showroom/admin.py
+++ car_showroom_app/src/car_showroom/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from src.car_showroom.models import Car_showroom, Car_showroom_Car
+
+@admin.register(Car_showroom)
+class ShowroomAdmin(admin.ModelAdmin):
+    list_display = ("name", "balance", "sortquery", "country")
+
+@admin.register(Car_showroom_Car)
+class CarSroomCarAdmin(admin.ModelAdmin):
+    list_display = ("count", "discount", "date", "car", "сar_showroom")
+
+
+
+# class Car_showroom_Car(models.Model):
+#     """buying a car from a provider
+#     """
+
+#     count = models.IntegerField(default=1)
+#     discount = models.IntegerField(
+#                           validators=[MinValueValidator(0),
+#                                       MaxValueValidator(100)])
+#     date = models.DateTimeField(auto_now=True)
+#     car = models.ForeignKey(Car, to_field='vin', on_delete=models.CASCADE)
+#     сar_showroom = models.ForeignKey(Car_showroom, on_delete=models.CASCADE)
+    
+
--- car_showroom_app/src/supplier/admin.py
+++ car_showroom_app/src/supplier/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from src.supplier.models import Supplier, Car, Supplier_Car
+
+
+@admin.register(Supplier)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'balance', )
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'width', 'mileage', 'price', 'vin' )
+
+
+
+@admin.register(Supplier_Car)
+class SupplierCarAdmin(admin.ModelAdmin):
+    list_display = ('count', 'discount', 'date', 'car', 'supplier' )
+
+
