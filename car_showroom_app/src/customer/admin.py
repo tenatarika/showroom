265aa47d6d437fd2ec7ce255e9a416c4426b3b4d
MacFurcas <faracsit@gmail.com>
2021-10-25 14:13:09 +0300

    migrations

--- car_showroom_app/src/customer/admin.py
+++ car_showroom_app/src/customer/admin.py
@@ -6,16 +6,16 @@ from django.db.models import Avg
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 'phone', 'birthday', 'balance', 'country', 'show_average')
+        'name', 'phone', 'birthday', 'balance', 'country', 'show_average',)
 
     def show_average(self, obj):
         result = Purchase.objects.filter(
             supplier=obj).aggregate(Avg("discount"))
         return result["discount__avg"]
 
-    show_average.short_description = "Average descount"
+    show_average.short_description = "Average discount"
 
 
 @admin.register(Purchase)
 class CustomerPurchaseAdmin(admin.ModelAdmin):
-    list_display = ('car', 'supplier', 'car_showroom', 'discount')
+    list_display = ('car', 'supplier', 'showroom', 'discount',)
