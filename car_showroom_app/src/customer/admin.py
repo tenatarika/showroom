9cbe23c50913323158222196bbd283f891dfb59e
MacFurcas <faracsit@gmail.com>
2021-11-04 11:42:34 +0300

    update admin.py

--- car_showroom_app/src/customer/admin.py
+++ car_showroom_app/src/customer/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
-from src.customer.models import Customer, Purchase
+from src.customer.models import Customer, Purchase, Location
 from django.db.models import Avg
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('country', 'city', 'street', 'houseNum',)
 
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 'phone', 'birthday', 'balance', 'country', 'show_average')
+        'name', 'phone', 'birthday', 'balance', 'location', 'show_average')
 
     def show_average(self, obj):
         result = Purchase.objects.filter(
             supplier=obj).aggregate(Avg("discount"))
-        return result["discount__avg"]
+        try:
+            return result["discount__avg"]
+        except AttributeError:
+            return None
 
-    show_average.short_description = "Average descount"
+    show_average.short_description = "Average discount"
 
 
 @admin.register(Purchase)
