3b7a6021af47db6a2d0b31e827dbc83753bdd628
MacFurcas <faracsit@gmail.com>
2021-11-05 16:13:09 +0300

    celery tasks

---
+++ car_showroom_app/src/car_showroom/services.py
@@ -0,0 +1,22 @@
+from src.car_showroom.models import CarShowroom, CarsOfShowroom
+from src.supplier.models import SupplierCar
+from config.celery import app
+
+
+@app.task
+def buy_cars() -> bool:
+
+    for showroom in CarShowroom.objects.all():
+        sample = showroom.sortquery
+        suppliers = SupplierCar.objects.filter(car__name=sample[0].get('name'))
+        for supplier in suppliers:
+            if showroom.balance > suppliers[0].car.price:
+                # print(showroom.balance, suppliers[0].supplier.balance)
+                showroom.balance -= suppliers[0].car.price
+                suppliers[0].supplier.balance += suppliers[0].car.price
+                pur = CarsOfShowroom.objects.create(discount=1, car=supplier.car, сar_showroom=showroom)
+                # print(showroom.balance, suppliers[0].supplier.balance)
+                pur.save()
+                showroom.save()
+                suppliers[0].supplier.save()
+                return True
---
+++ car_showroom_app/src/car_showroom/tasks.py
@@ -0,0 +1,9 @@
+from config.celery import app
+from src.car_showroom.services import buy_cars
+
+
+@app.task
+def showroom_task():
+    buy_cars.delay()
+
+
