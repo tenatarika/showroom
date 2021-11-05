9b4ce9d4e8132711f4e9df479fcceaf6df63ec71
MacFurcas <faracsit@gmail.com>
2021-11-05 16:12:40 +0300

    celery config

--- car_showroom_app/config/settings.py
+++ car_showroom_app/config/settings.py
@@ -208,3 +208,4 @@ CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+CELERY_IMPORTS = ("src.car_showroom.tasks",)
