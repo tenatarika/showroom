5095989aadd13b8e2cd2b10fd5c51281ad477915
tenatarika <72171218+tenatarika@users.noreply.github.com>
2021-11-11 14:49:05 +0300

    correct gender enum

--- car_showroom_app/src/car_showroom/services.py
+++ car_showroom_app/src/car_showroom/services.py
@@ -5,17 +5,15 @@ from config.celery import app
 
 @app.task
 def buy_cars() -> bool:
-
+    """Function for buying cars"""
     for showroom in CarShowroom.objects.all():
         sample = showroom.sortquery
         suppliers = SupplierCar.objects.filter(car__name=sample[0].get('name'))
         for supplier in suppliers:
             if showroom.balance >= suppliers[0].car.price:
-                # print(showroom.balance, suppliers[0].supplier.balance)
                 showroom.balance -= suppliers[0].car.price
                 suppliers[0].supplier.balance += suppliers[0].car.price
                 pur = CarsOfShowroom.objects.create(discount=1, car=supplier.car, сar_showroom=showroom)
-                # print(showroom.balance, suppliers[0].supplier.balance)
                 pur.save()
                 showroom.save()
                 suppliers[0].supplier.save()
